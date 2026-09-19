e0021fcb65b45c24511268f23a78a745f4ca267f
Sumit-20241087 <f20241087@pilani.bits-pilani.ac.in>
2026-09-19 16:33:32 +0000

    lab02 task5

--- labs/lab02/task5/alu.v
+++ labs/lab02/task5/alu.v
@@ -15,25 +15,25 @@
 module alu (
   input      [3:0] a,
   input      [3:0] b,
-  input             op,      // 0 = add, 1 = sub
+  input             op,
   output reg [3:0] result
 );
 
   reg [3:0] b_inv;
   reg [3:0] b_twos;
 
-  always @(a, b) begin
+  always @(a, b, op) begin
     case (op)
       1'b0: begin
-        result = a + b;                 // add
+        result = a + b;
       end
+
       1'b1: begin
-        b_inv  <= ~b;                   // sub, via two's complement
-        b_twos <= b_inv + 1;
-        result <= a + b_twos;
+        b_inv  = ~b;
+        b_twos = b_inv + 1;
+        result = a + b_twos;
       end
     endcase
   end
 
 endmodule
-
